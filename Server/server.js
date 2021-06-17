dfd700b712c9db9d029bd36b35be871a32ba1738
AU3 <usikov_andru@mail.ru>
2021-06-17 09:53:30 +0300

    Add socket rewrite

--- Server/server.js
+++ Server/server.js
@@ -1,78 +1,26 @@
-const http = require("http");
-const fs = require('fs').promises;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 8080;
 
-const port = '8080';
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+});
 
-var Message = "";
-var allMessages = [];
-var Num = 0;
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(__dirname + '/favicon.ico');
+});
 
-const requestListener = function (req, res) {
-    console.log(
-        `Request: ${req.method}, ${req.url}`
-    );
-    let fileName;
-    let contentType;
-    let IsMsg = false;
+app.get('/styles.css', (req, res) => {
+    res.sendFile(__dirname + '/styles.css');
+});
 
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
 
-    if (req.url === "/") {
-        fileName = "chat.html";
-        contentType = "text/html";
-    }
-    else if (req.url === "/newMessage") {
-        IsMsg = true;
-        res.writeHead(200);
-        let data = "";
-        req.on('data', (chunk) => {
-            data += chunk.toString();
-        })
-        req.on('end', () => {
-            console.log(`Reseived message: ${data}`);
-            if (data !== "") {
-                Message = data;
-                allMessages[Num++] = data;
-            }
-            res.end();
-        })
-        console.log(
-            `Request headers: ${JSON.stringify(req.headers)}`
-        );
-        return;
-    }
-    else if (req.url === "/getMessage") {
-        IsMsg = true;
-        res.writeHead(200);
-        res.end(JSON.stringify(allMessages));
-        console.log("Getting messages from user...");
-    }
-    else if (req.url === "/favicon.ico") {
-        fileName = "favicon.ico";
-        contentType = "image/x-icon";
-    }
-    else if (req.url.endsWith(".css")) {
-        fileName = req.url.substr(1);
-        contentType = "text/css";
-    } else {
-        res.writeHead(500);
-        res.end("Error, unsupported");
-        return;
-    }
-
-    if (!IsMsg) {
-        fs.readFile(`${__dirname}/${fileName}`)
-            .then(contents => {
-                res.setHeader("Content-Type", contentType);
-                res.writeHead(200);
-                res.end(contents.toString());
-            })
-            .catch(err => {
-                res.writeHead(500);
-                res.end(err.message);
-                return;
-            });
-    }
-};
-
-const server = http.createServer(requestListener);
-server.listen(port);
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
